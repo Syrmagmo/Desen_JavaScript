403d849ff93ae8770de71fe0617b59affe04cedd
syrmagmo <lincolnrichard2@gmail.com>
2022-07-14 22:41:45 -0300

    Create usandoVar1.js

---
+++ exercicios-js/usandoVar1.js
@@ -0,0 +1,17 @@
+{
+    {
+        {
+            {
+                var sera = 'sera???'
+                console.log(sera)
+            }
+        }
+        
+    }
+}
+
+console.log(sera)
+
+function teste (){
+    var local = 123
+}
